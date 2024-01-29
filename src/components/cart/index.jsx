ca6295c931452f57076983d4baa5f29b05371b1b
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-01-29 10:46:44 -0800

    cart

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,11 +1,11 @@
 import Layout from "../shared/layout"
 const Cart = ()=>{
     return (
-        <div>
             <Layout>
-                <h1>kaleem</h1>
+                <div>
+                    dfsdfsdf
+                </div>
             </Layout>
-        </div>
     )
 }
 
