c07263ee8b954315e2aea1d5908bfc42df4406ba
Kaleem Ullah <kaleemullahlakkimarwat123@gmail.com>
2024-01-29 10:34:10 -0800

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Layout from './components/shared/layout'
 import 'font-awesome/css/font-awesome.min.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Cart from './components/cart'
 const App = ()=>{
     return (
         <div>
                <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Layout />} />
+                    <Layout>
+                        <Route path='/cart' element={<Cart />} />
+                    </Layout>
                 </Routes>
                </BrowserRouter>
          </div>
